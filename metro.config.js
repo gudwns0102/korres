6fd279b6cae0b8145745ccd486e6f2073b665ad5
김형준 <brojun@gimhyeongjun-ui-MacBookAir.local>
2024-04-28 12:39:50 +0900

    connect korail-ts

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,18 @@
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
+const packagePath = "/Users/brojun/Desktop/works/korail-ts";
+
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    nodeModulesPaths: [packagePath],
+  },
+  watchFolders: [packagePath],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,46 @@
+import { css } from "@emotion/native";
+import { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
+import { useSession } from "../hooks/useSession";
+import { Async } from "../components/Async";
+
+export function LoginScreen() {
+  const session = useSession();
+  const [id, setId] = useState(process.env.KORAIL_ID || "");
+  const [password, setPassword] = useState(process.env.KORAIL_PW || "");
+
+  return (
+    <View
+      style={css`
+        flex: 1;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      `}
+    >
+      <TextInput
+        autoFocus
+        placeholder="멤버십 ID"
+        value={id}
+        onChangeText={setId}
+      />
+      <TextInput
+        placeholder="비밀번호"
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button
+        title="로그인"
+        onPress={() => {
+          session.login(id, password);
+        }}
+      />
+      <Text>
+        LoggedIn:{" "}
+        <Async fallback={<Text>Wait...</Text>}>
+          {session.checkLoggedIn().then(res => String(res))}
+        </Async>
+      </Text>
+    </View>
+  );
+}
