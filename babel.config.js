b27100b3f3acedc6aedb26673d7989591b136aad
김형준 <brojun@gimhyeongjun-ui-MacBookAir.local>
2024-05-03 11:03:10 +0900

    remove alias

--- babel.config.js
+++ babel.config.js
@@ -8,9 +8,6 @@ module.exports = {
       {
         root: ["./src"],
         extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
-        alias: {
-          "@": "./src",
-        },
       },
     ],
   ],
